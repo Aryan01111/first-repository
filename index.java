4a5f5a5d806eb096f6c488ff3656197598adb5b2
Aryan Sharma <Aryansh052003@gmail.com>
2023-04-13 23:36:11 +0530

    print sorted array

--- index.java
+++ index.java
@@ -1,5 +1,10 @@
+import java.util.*;
 public class index{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int [] a = {1,2,3,5,6,2,0,9};
+        Arrays.sort(a);
+        for(int i :a){
+            System.out.print(i+" ");
+        }
     }
 }
